39c61f4bb514ef39c1bad62b150e9415de2a71ea
AMENAB12 <lalatube71@gmail.com>
2023-08-21 15:49:16 +0200

    Feat:add ga eventTracker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Script from 'next/script'
+import ReactGA from 'react-ga';
 // components
 import Banner from './components/Banner';
 import Header from './components/Header';
@@ -8,18 +8,11 @@ import About from './components/About';
 import Services from './components/Services';
 import Work from './components/Work';
 import Contact from './components/Contact';
-
+const TRACKING_ID = "G-BB5S6SWCH5"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 const App = () => {
   return (
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
-       <Script id="google-analytics">
-        {`
-          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-          ga('create', 'UA-XXXXX-Y', 'auto');
-          ga('send', 'pageview');
-        `}
-      </Script>
-      <Script src="https://www.google-analytics.com/analytics.js" />
       <Header />
       <Banner />
       <Nav />
