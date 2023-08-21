4fa6e2c614948bcb26b6c0b651425e7a2b905b06
AMENAB12 <lalatube71@gmail.com>
2023-08-21 16:34:22 +0200

    send message tracker

--- src/util/useAnalyticsEventTracker.jsx
+++ src/util/useAnalyticsEventTracker.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactGA from "react-ga";
 
-const useAnalyticsEventTracker = (category="Blog category") => {
-  const eventTracker = (action = "test action", label = "test label") => {
+const useAnalyticsEventTracker = (category="Send Message") => {
+  const eventTracker = (action, label ) => {
     ReactGA.event({category, action, label});
   }
   return eventTracker;
