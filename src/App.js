0f1e2294f92842a86e0fcc272176a22bd03dbbf9
AMENAB12 <lalatube71@gmail.com>
2023-08-21 15:02:26 +0200

    fear:add Analytics Script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Script from 'next/script'
 // components
 import Banner from './components/Banner';
 import Header from './components/Header';
@@ -11,6 +12,14 @@ import Contact from './components/Contact';
 const App = () => {
   return (
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+       <Script id="google-analytics">
+        {`
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', 'UA-XXXXX-Y', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+      <Script src="https://www.google-analytics.com/analytics.js" />
       <Header />
       <Banner />
       <Nav />
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import Script from 'next/script'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
